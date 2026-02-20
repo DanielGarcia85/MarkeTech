4c905c4ed1738ff9ebaabbbfc28d092852ff2976
Daniel Garcia <danielgarcia2_1_85@hotmail.com>
2026-02-21 00:06:23 +0100

    test(ci/cd): deployment workflow

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,8 +3,8 @@ import Status from "./components/Status.vue";
 </script>
 
 <template>
-  <main style="max-width: 900px; margin: 24px auto; font-family: system-ui;">
-    <h1>MarkeTech</h1>
+  <main style="max-width: 900px; margin: 24px auto; font-family: system-ui">
+    <h1>MarkeTech - Test</h1>
     <Status />
   </main>
 </template>
